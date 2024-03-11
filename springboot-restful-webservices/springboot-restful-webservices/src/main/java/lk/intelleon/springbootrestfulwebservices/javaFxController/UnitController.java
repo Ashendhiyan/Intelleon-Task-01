663bcb832ac4aeddf7878b291a570a02dbc821a5
Ashendhiyan <ashen13516@gmail.com>
2024-03-12 00:54:04 +0530

    Unit form updated with secure all endpoints with JWT

--- springboot-restful-webservices/springboot-restful-webservices/src/main/java/lk/intelleon/springbootrestfulwebservices/javaFxController/UnitController.java
+++ springboot-restful-webservices/springboot-restful-webservices/src/main/java/lk/intelleon/springbootrestfulwebservices/javaFxController/UnitController.java
@@ -21,6 +21,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
 
+import static lk.intelleon.springbootrestfulwebservices.javaFxController.LoginController.authToken;
+
 @Component
 public class UnitController {
     @FXML
@@ -76,6 +78,8 @@ public class UnitController {
             URL url = new URL("http://localhost:8080/api/v1/unit");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
+            // Set authToken as a header
+            conn.setRequestProperty("Authorization", "Bearer " + authToken);
 
             // Get response code
             int responseCode = conn.getResponseCode();
@@ -116,74 +120,26 @@ public class UnitController {
 
     @FXML
     public void saveOnAction(ActionEvent actionEvent) {
-        if (isMatchUnitCode && isMatchUnitName){
-        try {
-            URL url = new URL("http://localhost:8080/api/v1/unit");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
-
-            // Create SupplierDTO object
-            UnitDTO unitDTO = new UnitDTO();
-            unitDTO.setCode(txtCode.getText());
-            unitDTO.setName(txtName.getText());
-            unitDTO.setStatus(cmbStatus.getValue());
-            //
-
-            // Convert SupplierDTO object to JSON
-            Gson gson = new Gson();
-            String jsonInputString = gson.toJson(unitDTO);
-
-            // Send JSON data
-            try (OutputStream os = conn.getOutputStream()) {
-                byte[] input = jsonInputString.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
-
-            // Get response code
-            int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Handle successful response
-                System.out.println("Unit saved successfully!");
-                showConfirmationMessage("Unit saved successfully!");
-                loadDataFromBackend();
-                clearTextFields();
-            } else {
-                // Handle error response
-                System.out.println("Error saving unit: " + responseCode);
-            }
-
-            conn.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        }else {
-            // Display an error message or handle the case where conditions are not met
-            showErrorAlert("Please ensure all fields are valid before saving.");
-        }
-    }
-
-    @FXML
-    public void updateOnAction(ActionEvent actionEvent) {
-        if (isMatchUnitCode && isMatchUnitName){
-        UnitDTO selectedUnit = tblUnit.getSelectionModel().getSelectedItem();
-        if (selectedUnit != null) {
+        if (isMatchUnitCode && isMatchUnitName) {
             try {
-                URL url = new URL("http://localhost:8080/api/v1/unit/" + selectedUnit.getId());
+                URL url = new URL("http://localhost:8080/api/v1/unit");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("PUT");
+                conn.setRequestMethod("POST");
                 conn.setRequestProperty("Content-Type", "application/json");
                 conn.setDoOutput(true);
+                // Set authToken as a header
+                conn.setRequestProperty("Authorization", "Bearer " + authToken);
 
-                // Create SupplierDTO object with updated details
-                selectedUnit.setCode(txtCode.getText());
-                selectedUnit.setName(txtName.getText());
-                selectedUnit.setStatus(cmbStatus.getValue());
+                // Create SupplierDTO object
+                UnitDTO unitDTO = new UnitDTO();
+                unitDTO.setCode(txtCode.getText());
+                unitDTO.setName(txtName.getText());
+                unitDTO.setStatus(cmbStatus.getValue());
+                //
 
                 // Convert SupplierDTO object to JSON
                 Gson gson = new Gson();
-                String jsonInputString = gson.toJson(selectedUnit);
+                String jsonInputString = gson.toJson(unitDTO);
 
                 // Send JSON data
                 try (OutputStream os = conn.getOutputStream()) {
@@ -195,14 +151,13 @@ public class UnitController {
                 int responseCode = conn.getResponseCode();
                 if (responseCode == HttpURLConnection.HTTP_OK) {
                     // Handle successful response
-                    System.out.println("Unit updated successfully!");
-                    showConfirmationMessage("Unit updated successfully!");
-                    // Refresh table data after update
+                    System.out.println("Unit saved successfully!");
+                    showConfirmationMessage("Unit saved successfully!");
                     loadDataFromBackend();
                     clearTextFields();
                 } else {
                     // Handle error response
-                    System.out.println("Error updating unit: " + responseCode);
+                    System.out.println("Error saving unit: " + responseCode);
                 }
 
                 conn.disconnect();
@@ -210,9 +165,62 @@ public class UnitController {
                 e.printStackTrace();
             }
         } else {
-            System.out.println("Please select a unit to update.");
+            // Display an error message or handle the case where conditions are not met
+            showErrorAlert("Please ensure all fields are valid before saving.");
         }
-        }else {
+    }
+
+    @FXML
+    public void updateOnAction(ActionEvent actionEvent) {
+        if (isMatchUnitCode && isMatchUnitName) {
+            UnitDTO selectedUnit = tblUnit.getSelectionModel().getSelectedItem();
+            if (selectedUnit != null) {
+                try {
+                    URL url = new URL("http://localhost:8080/api/v1/unit/" + selectedUnit.getId());
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("PUT");
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setDoOutput(true);
+                    // Set authToken as a header
+                    conn.setRequestProperty("Authorization", "Bearer " + authToken);
+
+                    // Create SupplierDTO object with updated details
+                    selectedUnit.setCode(txtCode.getText());
+                    selectedUnit.setName(txtName.getText());
+                    selectedUnit.setStatus(cmbStatus.getValue());
+
+                    // Convert SupplierDTO object to JSON
+                    Gson gson = new Gson();
+                    String jsonInputString = gson.toJson(selectedUnit);
+
+                    // Send JSON data
+                    try (OutputStream os = conn.getOutputStream()) {
+                        byte[] input = jsonInputString.getBytes("utf-8");
+                        os.write(input, 0, input.length);
+                    }
+
+                    // Get response code
+                    int responseCode = conn.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        // Handle successful response
+                        System.out.println("Unit updated successfully!");
+                        showConfirmationMessage("Unit updated successfully!");
+                        // Refresh table data after update
+                        loadDataFromBackend();
+                        clearTextFields();
+                    } else {
+                        // Handle error response
+                        System.out.println("Error updating unit: " + responseCode);
+                    }
+
+                    conn.disconnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Please select a unit to update.");
+            }
+        } else {
             // Display an error message or handle the case where conditions are not met
             showErrorAlert("Please ensure all fields are valid before updating.");
         }
@@ -226,6 +234,8 @@ public class UnitController {
                 URL url = new URL("http://localhost:8080/api/v1/unit/" + selectedUnit.getId());
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("DELETE");
+                // Set authToken as a header
+                conn.setRequestProperty("Authorization", "Bearer " + authToken);
 
                 // Get response code
                 int responseCode = conn.getResponseCode();
